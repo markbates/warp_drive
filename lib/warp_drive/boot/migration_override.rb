592e4d73cb078228ae29338b91282878e0c536ef
Mark Bates <mark@markbates.com>
2009-10-21 12:13:56 -0400

    Updated migration override to use alias_method_chain instead of redefining the migrations method directly.

--- lib/warp_drive/boot/migration_override.rb
+++ lib/warp_drive/boot/migration_override.rb
@@ -1,14 +1,14 @@
 module ActiveRecord # :nodoc:
   class Migrator # :nodoc:
   
-    migs = []
-    migs << ActiveRecord::Migrator.new(:up, WarpDrive::Path.db.migrate).migrations
-    migs << ActiveRecord::Migrator.new(:up, File.join(RAILS_ROOT, 'db', 'migrate')).migrations
-    migs.flatten!
-  
-    define_method('migrations') do
-      return migs
+    def migrations_with_warp_drive
+      migs = []
+      migs << ActiveRecord::Migrator.new(:up, WarpDrive::Path.db.migrate).migrations
+      migs << ActiveRecord::Migrator.new(:up, File.join(RAILS_ROOT, 'db', 'migrate')).migrations
+      migs.flatten
     end
+    
+    alias_method_chain :migrations, :warp_drive
   
   end
 end
